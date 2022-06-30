e07661f723ff622365329679840660146d9d6bae
Hıdır Yeşiltepe <70890453+yesiltepe-hidir@users.noreply.github.com>
2022-06-30 20:39:39 +0300

    Patch Embedding added

---
+++ models/basic_layers.py
@@ -0,0 +1,42 @@
+class PatchEmbedding(nn.Module):
+  '''
+  Given images are linearly embedded via Patch Embedding in order to get tokens.
+
+  Args:
+  -----  
+    img_size    [int]: Images are assumed to be square
+    patch_size  [int]: Images are divided into patches of size `patch size`
+    in_channels [int]: Number of input channels of given images
+    embed_dim   [int]: Final embedding dimension
+
+  Attributes:
+  -----------
+    n_patches   [int]:  Number of total patches at the end
+    projection  [Conv]: Patch extractor
+  '''
+
+  def __init__(self, img_size, patch_size, in_channels, embed_dim):
+    super().__init__()
+    self.img_size = img_size
+    self.patch_size = patch_size
+    self.in_channels = in_channels
+    self.embed_dim = embed_dim
+    
+    self.n_patches = (img_size // patch_size) ** 2
+    self.projection = nn.Conv2d(in_channels = in_channels, 
+                                out_channels = embed_dim, 
+                                kernel_size = patch_size, 
+                                stride = patch_size)
+  
+  def forward(self, x):
+    '''
+    Args:
+    -----
+      x [Tensor(B x C x H x W)]: Input images
+    
+    Returns:
+    --------
+      projected [Tensor(B x N x E)] where N stands for n_patches & E stands for embed_dim
+    '''
+    projected = self.projection(x).flatten(2).transpose(1, 2) 
+    return projected
